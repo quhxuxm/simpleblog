69dedf32a913bfdd06e09009920c18f4d9bd5c5e
Qu Hao <quhxuxm_quh@163.com>
2018-07-08 18:33:13 +0800

    init

--- repository/src/main/java/com/quhxuxm/quh/project/simpleblog/repository/view/IAuthorDetailViewMapper.java
+++
@@ -1,9 +0,0 @@
-package com.quhxuxm.quh.project.simpleblog.repository.view;
-
-import org.apache.ibatis.annotations.Param;
-
-import com.quhxuxm.quh.project.simpleblog.domain.view.AuthorDetail;
-
-public interface IAuthorDetailViewMapper {
-    AuthorDetail findOneById(@Param("id") long id);
-}
