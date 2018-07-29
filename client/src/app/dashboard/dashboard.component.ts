754b58e94af6d4a28dc51f7e2a3f6ff9638bd6b3
Qu Hao <quhxuxm_quh@163.com>
2018-07-30 07:18:52 +0800

    init

---
+++ client/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,31 @@
+import { Component, Inject } from '@angular/core';
+import { DataService } from '../data/data.service';
+import { Post } from '../Post';
+import { DataSource } from '@angular/cdk/table';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  constructor(@Inject('dataService') private dataService: DataService) {
+  }
+
+  displayedColumns = ['date_posted', 'title', 'category', 'delete'];
+  dataSource = new PostDataSource(this.dataService);
+}
+
+export class PostDataSource extends DataSource<any> {
+  constructor(private dataService: DataService) {
+    super();
+  }
+
+  connect(): Observable<Post[]> {
+    return this.dataService.getData();
+  }
+
+  disconnect() {
+  }
+}
