737dc304c0c0857b4b5e0dedb83b616ffb8244b0
Qu Hao <quhxuxm_quh@163.com>
2018-08-08 23:20:15 +0800

    move back to angular

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppUiModule} from './app.ui.module';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {NavigatorComponent} from './navigator/navigator.component';
+import {RegisterComponent} from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigatorComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    AppUiModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ client/src/app/app.ui.module.ts
@@ -0,0 +1,60 @@
+import {LayoutModule} from '@angular/cdk/layout';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fab} from '@fortawesome/free-brands-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas, far, fab);
+
+@NgModule({
+  imports: [
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    FormsModule,
+  ],
+  declarations: [],
+  exports: [
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    FormsModule,
+  ]
+})
+export class AppUiModule {
+}
